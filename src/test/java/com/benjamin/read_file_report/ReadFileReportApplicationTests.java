6e35342d23d142f37be4ecf3707a17ee6b0b3217
LAPTOP-4UHPN8A3\Benjamin Wong <benjamin_wong98@hotmail.com>
2024-11-17 21:50:57 +0800

    project launch

---
+++ src/test/java/com/benjamin/read_file_report/ReadFileReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.read_file_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadFileReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
