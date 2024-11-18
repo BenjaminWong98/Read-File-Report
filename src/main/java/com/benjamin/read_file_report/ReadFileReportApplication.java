5ac32c7f6ff2f04f2925e03febc7b315e4acffed
LAPTOP-4UHPN8A3\Benjamin Wong <benjamin_wong98@hotmail.com>
2024-11-18 22:05:23 +0800

    Model Project Launch

--- src/main/java/com/benjamin/read_file_report/ReadFileReportApplication.java
+++ src/main/java/com/benjamin/read_file_report/ReadFileReportApplication.java
@@ -1,8 +1,10 @@
 package com.benjamin.read_file_report;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class ReadFileReportApplication {
 
